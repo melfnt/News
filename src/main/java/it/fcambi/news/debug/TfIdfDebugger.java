27ce8c72469655ae1c20c2cd36bb5f3fbe7e897d
Lucio Messina <luciomessina94@gmail.com>
2017-01-20 17:58:59 +0100

    ver 0.5.1

--- src/main/java/it/fcambi/news/debug/TfIdfDebugger.java
+++ src/main/java/it/fcambi/news/debug/TfIdfDebugger.java
@@ -49,10 +49,10 @@ public class TfIdfDebugger
 	{
 		long id = a.getId();
 		try
-		{
-			log.info("conditional debugging on article with id="+id+". Its permutation is "+(  ( this._A * id + this._B ) % _DEBUG_ONE_ARTICLE_EVERY  ) );		
-			if (   (  ( this._A * id + this._B ) % _DEBUG_ONE_ARTICLE_EVERY  ) == 0  &&  ! already_debugged.contains (id)   )
+		{		
+			if (   (  ( this._A * id + this._B ) % _DEBUG_ONE_ARTICLE_EVERY  ) == 3  &&  ! already_debugged.contains (id)   )
 			{
+				log.info("conditional debugging on article with id="+id+". Its permutation is "+(  ( this._A * id + this._B ) % _DEBUG_ONE_ARTICLE_EVERY  ) );
 				FileWriter fw = new FileWriter( _DEBUG_DIRECTORY + "article_" + id + filename_suffix );
 				List <Double> values = vector.getValues ();
 				List <String> words = vector.getWords ();
